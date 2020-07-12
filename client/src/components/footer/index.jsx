4c797d439e7d8b38e83199eebe2ec476563d057f
Adam Jackson <akjackson1@gmail.com>
2020-07-12 10:02:10 -0400

    Update footer and meta tag descriptions

--- client/src/components/footer/index.jsx
+++ client/src/components/footer/index.jsx
@@ -8,12 +8,12 @@ function Footer() {
 			<div className="container">
 				<div className="siteinfo">
 					<h2>PuffinCloud.org</h2>
-					<p>Morbi convallis bibendum urna ut viverra. Maecenas quis consequat libero, a feugiat eros. Nunc ut lacinia tortor morbi ultricies laoreet ullamcorper phasellus semper.</p>
+					<p>All images are published under the Creative Commons Zero license, making bird images freely availabe for anyone to use. We hope to make it easier for organizations and individuals to advocate for the preservation of avian biodiversity and the environment. </p>
 				</div>
 				<div className="info-links">
 					<Link to="/about">About</Link>	
 					<Link to="/license">License</Link>
-					<a href="">Contribute</a>
+					<Link to="/sign-up">Contribute</Link>
 				</div>
 			</div>
 		</footer>
