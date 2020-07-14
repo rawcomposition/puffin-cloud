802b2acfbb5e5ede8d999eb9d9f5b2998cb6d51a
Adam Jackson <akjackson1@gmail.com>
2020-07-14 14:40:20 -0400

    Refactor Profile page to use Axios instead of GraphQL

--- client/src/pages/profilepage/index.jsx
+++ client/src/pages/profilepage/index.jsx
@@ -1,47 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PhotoList from '../../components/photo-list/photo-list.component';
 import Avatar from '../../components/avatar';
-import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
 import UserStats from '../../components/user-stats';
 import { setTitle } from '../../utils/global';
 import Error404 from '../error-404';
+import axios from 'axios';
 import './styles.scss';
 
-const USER_QUERY = gql`
-	query User($userId: ID!){
-		user(id: $userId) {
-			first_name,
-			last_name,
-			id,
-			avatar
-		}
-	}
-`;
 function ProfilePage({match: {params: {userId}}}) {
+	const [user, setUser] = useState(null);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(false);
+
+	useEffect(() => {
+		axios.get('users/' + userId)
+		.then(response => {
+			setUser(response.data);
+			setTitle(response.data.first_name + ' ' + response.data.last_name);
+		})
+		.catch(error => {
+			setError(true);
+		})
+		.then(() => {
+			setLoading(false);
+		})
+	}, []);
+
+	if (loading) return "loading...";
+	if (error || !user) return <Error404/>;
 	return (
 		<div className="container profile-page">
 			<div className="profile">
-				<Query query={USER_QUERY} variables={{userId}}>
-					{({ loading, error, data }) => {
-						if (loading) return "Loading stats..."
-						if (error || ! data.user) return <Error404/>
-						if (data.user) {
-							setTitle(data.user.first_name + ' ' + data.user.last_name);
-							return (
-								<div className="profile">
-									<Avatar url={data.user.avatar} size="100"/>
-									<span className="profile-info">
-										<h3>{data.user.first_name} {data.user.last_name}</h3>
-										<UserStats userId={data.user.id}/>
-									</span>
-								</div>
-							);
-						} else {
-							return <p>User not found</p>
-						}
-					}}
-				</Query>
+				<div className="profile">
+					<Avatar url={user.avatar} size="100"/>
+					<span className="profile-info">
+						<h3>{user.first_name} {user.last_name}</h3>
+						<UserStats userId={user.id}/>
+					</span>
+				</div>
 			</div>
 			<PhotoList userId={userId}/>
 		</div>
