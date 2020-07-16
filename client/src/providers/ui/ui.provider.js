74e92717cdfc367b4dcb3a87327ee03a30ca573d
Adam Jackson <akjackson1@gmail.com>
2020-07-16 07:06:23 -0400

    Clean up tabbing in UI context

--- client/src/providers/ui/ui.provider.js
+++ client/src/providers/ui/ui.provider.js
@@ -1,20 +1,20 @@
 import React, { createContext, useState } from 'react';
 
 export const UIContext = createContext({
-  loginModalHidden: true,
-  toggleLoginModal: () => {},
+	loginModalHidden: true,
+	toggleLoginModal: () => {},
 });
 
 const UIProvider = ({ children }) => {
-  const [loginModalHidden, setLoginModalHidden] = useState(true);
+	const [loginModalHidden, setLoginModalHidden] = useState(true);
 
-  const toggleLoginModal = () => setLoginModalHidden(!loginModalHidden);
+	const toggleLoginModal = () => setLoginModalHidden(!loginModalHidden);
 
-  return (
-    <UIContext.Provider value={{ loginModalHidden, toggleLoginModal }}>
-      {children}
-    </UIContext.Provider>
-  );
+	return (
+		<UIContext.Provider value={{ loginModalHidden, toggleLoginModal }}>
+			{children}
+		</UIContext.Provider>
+	);
 };
 
 export default UIProvider;
