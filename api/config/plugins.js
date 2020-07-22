1dd7760737e44a2bb6b4202b851c9f3b4a9462e7
Adam Jackson <akjackson1@gmail.com>
2020-07-21 20:02:23 -0400

    Update default SendGrid email to non-personal email

--- api/config/plugins.js
+++ api/config/plugins.js
@@ -16,8 +16,8 @@ module.exports = ({ env }) => ({
 			apiKey: env('SENDGRID_API_KEY'),
 		},
 		settings: {
-			defaultFrom: 'akjackson1@gmail.com',
-			defaultReplyTo: 'akjackson1@gmail.com',
+			defaultFrom: 'info@puffincloud.org',
+			defaultReplyTo: 'info@puffincloud.org',
 		},
 	},
   });
