c32cdef24b686ff2ab49adbb4064cb43f1d53309
Adam Jackson <akjackson1@gmail.com>
2020-08-04 08:30:19 -0400

    Remove hard-coded localhost URL

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import UserProvider from './providers/user/user.provider';
 import axios from 'axios';
 import { getJWT } from './utils/user';
 
-axios.defaults.baseURL = 'http://localhost:1337/';
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 const jwt = getJWT();
 if(jwt) {
 	axios.defaults.headers.common['Authorization'] = `Bearer ${jwt}`;
